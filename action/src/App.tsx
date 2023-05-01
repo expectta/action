fa4f6b2bbe0b434686eab382a4dac6de1f3f09e6
jiwoon <expect.ta@mail.com>
2023-05-01 18:52:49 +0900

    불필요코드 삭제

--- action/src/App.tsx
+++ action/src/App.tsx
@@ -13,14 +13,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>이게 마지막이다</p>
         <p>{port}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
